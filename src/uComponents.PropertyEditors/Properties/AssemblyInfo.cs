1857b14e054015a3b0e0c5abc04769a32d8f6861
leekelleher <leekelleher@gmail.com>
2014-01-20 20:15:43 +0000

    Added a project for Property Editors

---
+++ src/uComponents.PropertyEditors/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("uComponents.PropertyEditors")]
+[assembly: AssemblyDescription("uComponents Property Editors for Umbraco 7+")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("The Umbraco Community")]
+[assembly: AssemblyProduct("uComponents")]
+[assembly: AssemblyCopyright("Copyright \xa9 The Umbraco Community 2014")]
+[assembly: AssemblyTrademark("The Umbraco Community")]
+[assembly: AssemblyCulture("")]
