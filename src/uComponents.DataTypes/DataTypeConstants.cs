1e07d9ccc9a83de2700309fb9aba5ed8e622b84a
leekelleher <leekelleher@gmail.com>
2014-01-21 19:48:30 +0000

    Re-added the `DataTypeGuids` constants class.
    As I broke backwards-compatibility.

---
+++ src/uComponents.DataTypes/DataTypeConstants.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using uComponents.Core;
+
+namespace uComponents.DataTypes
+{
+	/// <summary>
+	/// Defines the Guids for all the DataTypes; e.g. quick access to 3rd-party developers.
+	/// </summary>
+	[Obsolete("This class is now obsolete please use uComponents.Core.Constants.DataTypes", false)]
+	public class DataTypeConstants
+	{
+		/// <summary>
+		/// Guid for the AutoComplete data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.AutoCompleteId", false)]
+		public const string AutoCompleteId = Constants.DataTypes.AutoCompleteId;
+
+		/// <summary>
+		/// Guid for the CharLimit data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.CharLimitId", false)]
+		public const string CharLimitId = Constants.DataTypes.CharLimitId;
+
+		/// <summary>
+		/// Guid for the CheckBoxTree data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.CheckBoxTreeId", false)]
+		public const string CheckBoxTreeId = Constants.DataTypes.CheckBoxTreeId;
+
+		/// <summary>
+		/// Guid for the CountryPicker data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.CountryPickerId", false)]
+		public const string CountryPickerId = Constants.DataTypes.CountryPickerId;
+
+		/// <summary>
+		/// Guid for the DataTypeGrid data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.DataTypeGridId", false)]
+		public const string DataTypeGridId = Constants.DataTypes.DataTypeGridId;
+
+		/// <summary>
+		/// Guid for the DropdownCheckList data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.DropdownCheckListId", false)]
+		public const string DropdownCheckListId = Constants.DataTypes.DropdownCheckListId;
+
+		/// <summary>
+		/// Guid for the ElasticTextBox data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.ElasticTextBoxId", false)]
+		public const string ElasticTextBoxId = Constants.DataTypes.ElasticTextBoxId;
+
+		/// <summary>
+		/// Guid for the EnumCheckBoxList data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.EnumCheckBoxListId", false)]
+		public const string EnumCheckBoxListId = Constants.DataTypes.EnumCheckBoxListId;
+
+		/// <summary>
+		/// Guid for the EnumDropDownList data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.EnumDropDownListId", false)]
+		public const string EnumDropDownListId = Constants.DataTypes.EnumDropDownListId;
+
+		/// <summary>
+		/// Guid for the FileDropDownList data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.FileDropDownListId", false)]
+		public const string FileDropDownListId = Constants.DataTypes.FileDropDownListId;
+
+		/// <summary>
+		/// Guid for the FilePicker data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.FilePickerId", false)]
+		public const string FilePickerId = Constants.DataTypes.FilePickerId;
+
+		/// <summary>
+		/// Guid for the ImageDropdown data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.ImageDropdownId", false)]
+		public const string ImageDropdownId = Constants.DataTypes.ImageDropdownId;
+
+		/// <summary>
+		/// Guid for the Image Point data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.ImagePointId", false)]
+		public const string ImagePointId = Constants.DataTypes.ImagePointId;
+
+		/// <summary>
+		/// Guid for the IncrementalTextBox data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.IncrementalTextBoxId", false)]
+		public const string IncrementalTextBoxId = Constants.DataTypes.IncrementalTextBoxId;
+
+		/// <summary>
+		/// Guid for the JsonDropdown data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.JsonDropdownId", false)]
+		public const string JsonDropdownId = Constants.DataTypes.JsonDropdownId;
+
+		/// <summary>
+		/// Guid for the MultiNodeTreePicker data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.MultiNodeTreePickerId", false)]
+		public const string MultiNodeTreePickerId = Constants.DataTypes.MultiNodeTreePickerId;
+
+		/// <summary>
+		/// Guid for the MultiPickerRelations data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.MultiPickerRelationsId", false)]
+		public const string MultiPickerRelationsId = Constants.DataTypes.MultiPickerRelationsId;
+
+		/// <summary>
+		/// Guid for the MultipleDates data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.MultipleDatesId", false)]
+		public const string MultipleDatesId = Constants.DataTypes.MultipleDatesId;
+
+		/// <summary>
+		/// Guid for the MultipleTextstring data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.MultipleTextstringId", false)]
+		public const string MultipleTextstringId = Constants.DataTypes.MultipleTextstringId;
+
+		/// <summary>
+		/// Guid for the MultiUrlPicker data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.MultiUrlPickerId", false)]
+		public const string MultiUrlPickerId = Constants.DataTypes.MultiUrlPickerId;
+
+		/// <summary>
+		/// Guid for the Notes data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.NotesId", false)]
+		public const string NotesId = Constants.DataTypes.NotesId;
+
+		/// <summary>
+		/// Guid for the PropertyPicker data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.PropertyPickerId", false)]
+		public const string PropertyPickerId = Constants.DataTypes.PropertyPickerId;
+
+		/// <summary>
+		/// Guid for the RadioButtonListWithImages data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.RadioButtonListWithImagesId", false)]
+		public const string RadioButtonListWithImagesId = Constants.DataTypes.RadioButtonListWithImagesId;
+
+		/// <summary>
+		/// Guid for the RelatedLinksWithMedia data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.RelatedLinksWithMediaId", false)]
+		public const string RelatedLinksWithMediaId = Constants.DataTypes.RelatedLinksWithMediaId;
+
+		/// <summary>
+		/// Guid for the RelationLinks data-type
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.RelationLinks", false)]
+		public const string RelationLinks = Constants.DataTypes.RelationLinks;
+
+		/// <summary>
+		/// Guid for the RenderMacro data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.RenderMacroId", false)]
+		public const string RenderMacroId = Constants.DataTypes.RenderMacroId;
+
+		/// <summary>
+		/// Guid for the Similarity data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.SimilarityId", false)]
+		public const string SimilarityId = Constants.DataTypes.SimilarityId;
+
+		/// <summary>
+		/// Guid for the Slider data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.SliderId", false)]
+		public const string SliderId = Constants.DataTypes.SliderId;
+
+		/// <summary>
+		/// Guid foor the SqlAutoComplete data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.SqlAutoCompleteId", false)]
+		public const string SqlAutoCompleteId = Constants.DataTypes.SqlAutoCompleteId;
+
+		/// <summary>
+		/// Guid for the SqlCheckBoxList data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.SqlCheckBoxListId", false)]
+		public const string SqlCheckBoxListId = Constants.DataTypes.SqlCheckBoxListId;
+
+		/// <summary>
+		/// Guid for the SqlDropDownList data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.SqlDropDownListId", false)]
+		public const string SqlDropDownListId = Constants.DataTypes.SqlDropDownListId;
+
+		/// <summary>
+		/// Guid for the StyledTextBox data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.StyledTextBoxId", false)]
+		public const string StyledTextBoxId = Constants.DataTypes.StyledTextBoxId;
+
+		/// <summary>
+		/// Guid for the SubTabs data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.SubTabs", false)]
+		public const string SubTabs = Constants.DataTypes.SubTabs;
+
+		/// <summary>
+		/// Guid for the TextImage data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.TextImageId", false)]
+		public const string TextImageId = Constants.DataTypes.TextImageId;
+
+		/// <summary>
+		/// Guid for the TextstringArray data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.TextstringArrayId", false)]
+		public const string TextstringArrayId = Constants.DataTypes.TextstringArrayId;
+
+		/// <summary>
+		/// Guid for the ToggleBox data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.ToggleBoxId", false)]
+		public const string ToggleBoxId = Constants.DataTypes.ToggleBoxId;
+
+		/// <summary>
+		/// Guid for the UniqueProperty data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.UniquePropertyId", false)]
+		public const string UniquePropertyId = Constants.DataTypes.UniquePropertyId;
+
+		/// <summary>
+		/// Guid for the UrlPicker data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.UrlPickerId", false)]
+		public const string UrlPickerId = Constants.DataTypes.UrlPickerId;
+
+		/// <summary>
+		/// Guid for the UserPicker data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.UserPickerId", false)]
+		public const string UserPickerId = Constants.DataTypes.UserPickerId;
+
+		/// <summary>
+		/// Guid for the XmlDropDownListId data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.XmlDropDownListId", false)]
+		public const string XmlDropDownListId = Constants.DataTypes.XmlDropDownListId;
+
+		/// <summary>
+		/// Guid for the XPathAutoComplete data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.XPathAutoCompleteId", false)]
+		public const string XPathAutoCompleteId = Constants.DataTypes.XPathAutoCompleteId;
+
+		/// <summary>
+		/// Guid for the XPathCheckBoxList data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.XPathCheckBoxListId", false)]
+		public const string XPathCheckBoxListId = Constants.DataTypes.XPathCheckBoxListId;
+
+		/// <summary>
+		/// Guid for the XPathChildNodePicker data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.XPathChildNodePickerId", false)]
+		public const string XPathChildNodePickerId = Constants.DataTypes.XPathChildNodePickerId;
+
+		/// <summary>
+		/// Guid for the XPathDropDownList data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.XPathDropDownListId", false)]
+		public const string XPathDropDownListId = Constants.DataTypes.XPathDropDownListId;
+
+		/// <summary>
+		/// Guid for the XPathRadioButtonList data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.XPathRadioButtonListId", false)]
+		public const string XPathRadioButtonListId = Constants.DataTypes.XPathRadioButtonListId;
+
+		/// <summary>
+		/// Guid for the XPathTemplatableList data-type.
+		/// </summary>
+		[Obsolete("This property is now obsolete please use uComponents.Core.Constants.DataTypes.XPathTemplatableListId", false)]
+		public const string XPathTemplatableListId = Constants.DataTypes.XPathTemplatableListId;
+	}
+}
