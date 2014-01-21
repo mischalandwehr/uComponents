4d061f2b330c0282716b65dc327a3bb78211d713
leekelleher <leekelleher@gmail.com>
2014-01-21 19:23:45 +0000

    Added a stub class for the PropertyEditor alias constants.
    Wrapped the DataType ID constants in the `Constants` static class/helper.

---
+++ src/uComponents.Core/Constants-PropertyEditors.cs
@@ -0,0 +1,15 @@
+namespace uComponents.Core
+{
+	/// <summary>
+	/// Constants class for uComponent specific values.
+	/// </summary>
+	public static partial class Constants
+	{
+		/// <summary>
+		/// Defines the aliases for all the PropertyEditors; e.g. quick access to 3rd-party developers.
+		/// </summary>
+		public static class PropertyEditors
+		{
+		}
+	}
+}
